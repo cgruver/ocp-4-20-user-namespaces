f0e98586ffcfafd0142488394646e3df61385534
cgruver <cgruver@redhat.com>
2025-11-07 15:17:13 +0000

    init

---
+++ workspace-image/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ ! -d "${HOME}" ]
+then
+  mkdir -p "${HOME}"
+fi
+
+if [ ! -d "${HOME}/.config/containers" ]
+then
+  mkdir -p ${HOME}/.config/containers
+  (echo '[storage]';echo 'driver = "overlay"';echo 'graphroot = "/tmp/graphroot"';echo '[storage.options.overlay]';echo 'mount_program = "/usr/bin/fuse-overlayfs"') > ${HOME}/.config/containers/storage.conf
+fi
+
+exec "$@"
